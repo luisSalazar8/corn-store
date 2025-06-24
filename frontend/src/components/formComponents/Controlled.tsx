ec92f00b47bd69c04c459ba5d3c3761a0f2f24c9
LuisS <luis8e@hotmail.com>
2025-06-24 16:15:56 -0500

    feat: login form

---
+++ frontend/src/components/formComponents/Controlled.tsx
@@ -0,0 +1,42 @@
+import { cloneElement } from "react";
+import {
+  Controller,
+  useFormContext,
+  type RegisterOptions,
+} from "react-hook-form";
+
+interface ControlledProps {
+  name: string;
+  input: React.ReactElement;
+  rules?: RegisterOptions;
+}
+
+const Controlled = ({ name, input, rules }: ControlledProps) => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <div>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => {
+          return cloneElement(input, {
+            ...field,
+            ...(input.props || {}),
+          });
+        }}
+        rules={rules}
+      />
+      {errors[name] && (
+        <span className="text-red-500 text-sm">
+          {errors[name]?.message as string}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default Controlled;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -4,44 +4,69 @@ import StyledInput from "@/components/styledComponents/StyledInput";
 import SuccessButton from "@/components/styledComponents/SuccessButton";
 import { CardContent, CardFooter, CardTitle } from "@/components/ui/card";
 import { Link } from "react-router";
+import { FormProvider, useForm } from "react-hook-form";
+import Controlled from "@/components/formComponents/Controlled";
+
+interface LoginForm {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
+  const methods = useForm<LoginForm>();
+  const { handleSubmit } = methods;
+  const onSubmit = (data: LoginForm) => console.log(data);
+
   return (
     <div className="my-20">
-      <div className="max-w-[520px] mx-auto">
-        <StyledCard>
-          <CardTitle className="mx-auto text-3.5xl font-semibold text-gray-footer">
-            Sign In
-          </CardTitle>
-          <CardContent>
-            <form>
-              <div className="flex flex-col gap-3">
-                <StyledInput
-                  id="email"
-                  type="email"
-                  placeholder="Email"
-                  required
-                />
-                <PasswordInput id="password" placeholder="Password" required />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="max-w-[520px] mx-auto">
+          <StyledCard>
+            <CardTitle className="mx-auto text-3.5xl font-semibold text-gray-footer">
+              Sign In
+            </CardTitle>
+
+            <CardContent>
+              <FormProvider {...methods}>
+                <div className="flex flex-col gap-3">
+                  <Controlled
+                    name="email"
+                    input={
+                      <StyledInput
+                        id="email"
+                        type="email"
+                        placeholder="Email"
+                      />
+                    }
+                    rules={{ required: true }}
+                  />
+                  <Controlled
+                    name="password"
+                    input={
+                      <PasswordInput id="password" placeholder="Password" />
+                    }
+                    rules={{ required: true }}
+                  />
+                </div>
+              </FormProvider>
+            </CardContent>
+            <CardFooter className="flex-col gap-2">
+              <SuccessButton type="submit">Login</SuccessButton>
+              <div className="text-gray-link text-sm">
+                Don’t have account?
+                <span className="text-gray-footer ml-1 text-sm">
+                  <Link
+                    to="/register"
+                    className="underline-offset-4 hover:underline hover:decoration-gray-footer"
+                  >
+                    Register
+                  </Link>
+                </span>
               </div>
-            </form>
-          </CardContent>
-          <CardFooter className="flex-col gap-2">
-            <SuccessButton type="submit">Login</SuccessButton>
-            <div className="text-gray-link text-sm">
-              Don’t have account?
-              <span className="text-gray-footer ml-1 text-sm">
-                <Link
-                  to="/register"
-                  className="underline-offset-4 hover:underline hover:decoration-gray-footer"
-                >
-                  Register
-                </Link>
-              </span>
-            </div>
-          </CardFooter>
-        </StyledCard>
-      </div>
+            </CardFooter>
+          </StyledCard>
+        </div>
+      </form>
     </div>
   );
 };
