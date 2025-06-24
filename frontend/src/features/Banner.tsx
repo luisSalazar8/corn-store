6c157f3e9fce9a219f0420ce22e130e834946dc1
LuisS <luis8e@hotmail.com>
2025-06-24 09:15:43 -0500

    feat: base elements

---
+++ frontend/src/features/Banner.tsx
@@ -0,0 +1,11 @@
+import Breadcrumb from "../assets/images/Breadcrumbs.png";
+
+const Banner = () => {
+  return (
+    <div>
+      <img src={Breadcrumb} alt="banner" />
+    </div>
+  );
+};
+
+export default Banner;
---
+++ frontend/src/features/Header.tsx
@@ -0,0 +1,17 @@
+import Icon from "../assets/images/Icon.png";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="bg-gray-header text-gray-clear text-xs py-3 px-75">
+        <div className="w-fit ml-auto">Sign In / Sign Up</div>
+      </div>
+      <div className="px-75 flex items-center gap-2">
+        <img src={Icon} alt="logo" className="size-[32px]" />
+        <span className="text-3.5xl">Bob's Farm</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
