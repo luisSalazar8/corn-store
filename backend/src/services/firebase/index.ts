3fce9eee8613e99e17b1b841f3dc6a4afa3da888
LuisS <luis8e@hotmail.com>
2025-06-24 06:46:10 -0500

    fix: use correct env variable for firebase

--- backend/src/services/firebase/index.ts
+++ backend/src/services/firebase/index.ts
@@ -5,7 +5,7 @@ import fs from "fs";
 function initFirebase() {
   if (admin.apps.length > 0) return;
 
-  const relativePath = process.env.relativePath;
+  const relativePath = process.env.FIREBASE_CERT;
   const keyPath = path.resolve(relativePath || "");
   if (!relativePath || !keyPath || !fs.existsSync(keyPath)) {
     throw new Error("firebase cert file is missing or invalid");
