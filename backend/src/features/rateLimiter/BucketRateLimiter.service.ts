e73f3f909efb9625fe68d5f4096254e472e1e040
LuisS <luis8e@hotmail.com>
2025-06-25 04:04:09 -0500

    fix: improved rate limiter logic

--- backend/src/features/rateLimiter/BucketRateLimiter.service.ts
+++ backend/src/features/rateLimiter/BucketRateLimiter.service.ts
@@ -4,38 +4,56 @@ import { BucketRateLimit } from "../../database/entities/mainDB/BucketRateLimit"
 export async function CheckLimit(userID: string): Promise<boolean> {
   const refillRate = Number(process.env.RL_BUCKET_REFILL);
   const bucketMaxSize = Number(process.env.RL_BUCKET_SIZE);
-  const bucketRLRepository = AppSource.getRepository(BucketRateLimit);
 
-  let bucketRecord = await bucketRLRepository.findOneBy({
-    user: { id: userID },
-  });
-  if (!bucketRecord) {
-    const newBucketRecord = bucketRLRepository.create({
-      user: { id: userID },
-      tokensUsed: 1,
-      refillTimestamp: new Date(),
-    });
-
-    await bucketRLRepository.save(newBucketRecord);
-    return true;
+  if (isNaN(refillRate) || isNaN(bucketMaxSize)) {
+    throw new Error("Rate limit config missing or invalid.");
   }
 
-  const current = Date.now();
-  const refillTimestamp = bucketRecord.refillTimestamp;
-  if (current - refillTimestamp.getTime() > refillRate) {
-    bucketRecord.refillTimestamp = new Date(current);
-    bucketRecord.tokensUsed = 1;
+  const hasTokenAccess = await AppSource.transaction(async (manager) => {
+    const bucketRLRepository = manager.getRepository(BucketRateLimit);
+
+    const bucketRecord = await bucketRLRepository.findOne({
+      where: { user: { id: userID } },
+      lock: { mode: "pessimistic_write" },
+    });
+
+    if (!bucketRecord) {
+      const newBucketRecord = bucketRLRepository.create({
+        user: { id: userID },
+        tokensUsed: 1,
+        refillTimestamp: new Date(),
+      });
+
+      await bucketRLRepository.save(newBucketRecord);
+      return true;
+    }
+
+    const current = Date.now();
+    const refillTimestamp = bucketRecord.refillTimestamp;
+    const elapsedTime = current - refillTimestamp.getTime();
+    if (elapsedTime > refillRate) {
+      const newTokensBasedOnTime = Math.floor(elapsedTime / refillRate);
+      if (newTokensBasedOnTime > 0) {
+        bucketRecord.tokensUsed = Math.max(
+          bucketRecord.tokensUsed - newTokensBasedOnTime,
+          0
+        );
+      }
+
+      bucketRecord.refillTimestamp = new Date(
+        refillTimestamp.getTime() + newTokensBasedOnTime * refillRate
+      );
+    }
+
+    if (bucketRecord.tokensUsed >= bucketMaxSize) {
+      return false;
+    }
+
+    bucketRecord.tokensUsed += 1;
     await bucketRLRepository.save(bucketRecord);
 
     return true;
-  }
+  });
 
-  const tokensUsed = bucketRecord.tokensUsed;
-  if (tokensUsed >= bucketMaxSize) {
-    return false;
-  }
-
-  bucketRecord.tokensUsed += 1;
-  await bucketRLRepository.save(bucketRecord);
-  return true;
+  return hasTokenAccess;
 }
