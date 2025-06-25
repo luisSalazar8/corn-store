e0f6696550d7ffa6705c4b50a8f5da53c1d53984
LuisS <luis8e@hotmail.com>
2025-06-24 23:29:57 -0500

    fix: capitalize name

--- frontend/src/features/products/Item.tsx
+++ frontend/src/features/products/Item.tsx
@@ -10,7 +10,7 @@ const Item = ({ item }: ItemProps) => {
     <div className="group border-gray-input border border-solid w-fit hover:border-success-dark hover:shadow-card-success">
       <img src={item.image} className="max-w-[264px] h-auto max-h-[240px]" />
       <div className="flex justify-around p-3 items-center">
-        <div className="text-sm text-gray-name group-hover:text-success-dark">
+        <div className="capitalize text-sm text-gray-name group-hover:text-success-dark">
           {item.name}
         </div>
         <button className="group/icon size-[40px] rounded-full bg-gray-card flex justify-center items-center hover:cursor-pointer active:bg-success transition duration-150">
